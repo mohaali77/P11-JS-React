d67aa80debfd1d2f5990292d330eaf40e18dcb45
mohaali77 <mzr.mohand@gmail.com>
2023-07-19 16:58:25 +0200

    modif

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import '../src/styles/footer.css'
-import '../src/styles/header.css'
-import '../src/styles/errorComponent.css'
-import '../src/styles/banner.css'
-import '../src/styles/card.css'
+import '../src/styles/footer/footer.css'
+import '../src/styles/header/header.css'
+import '../src/styles/error/errorComponent.css'
+import '../src/styles/banner/banner.css'
+import '../src/styles/card/card.css'
 import './App.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
