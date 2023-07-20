553a2d13aaaeb1d971bde74054ac97f2a1ecad09
mohaali77 <mzr.mohand@gmail.com>
2023-07-21 00:29:31 +0200

    modif

--- my-app/src/components/Card.jsx
+++ my-app/src/components/Card.jsx
@@ -1,6 +1,8 @@
-export function Card() {
+export function Card({ data, image }) {
+    console.log(data);
+    console.log(data.id);
     return <section id='cardSection'>
-        <div className='card'></div>
+        <div className='card'>aaa{data.title}</div>
         <div className='card'></div>
         <div className='card'></div>
         <div className='card'></div>
--- my-app/src/pages/Home/Home.jsx
+++ my-app/src/pages/Home/Home.jsx
@@ -3,11 +3,13 @@ import { Card } from "../../components/Card"
 import image1 from '../../images/Image1.png'
 import "../../pages/Home/style/banner_home.css"
 import "../../pages/Home/style/card.css"
+import data from '../../data/data.json'
 const sentance = 'Chez vous, partout et ailleurs'
+
 
 export function Home() {
     return <div>
         <Banner sentance={sentance} image={image1} />
-        <Card />
+        <Card data={data} image={image1} />
     </div>
 }
