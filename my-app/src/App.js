95ce153be585e80e390c2faeed99672ba3d08074
mohaali77 <mzr.mohand@gmail.com>
2023-07-23 22:43:35 +0200

    modif

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/about" element={<About />} />
-    <Route path="/details" element={<HouseDetails />} />
+    <Route path="/details/:id" element={<HouseDetails />} />
     <Route path="*" element={<Error />} />
   </Routes>
 }
