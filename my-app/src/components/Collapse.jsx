2d823ef29d263c41f4cbc6afef09584383c66b5e
mohaali77 <mzr.mohand@gmail.com>
2023-07-31 01:03:00 +0200

    modif

---
+++ my-app/src/components/Collapse.jsx
@@ -0,0 +1,13 @@
+export function Collapse() {
+    return <>
+        <div id="collapse">
+            <div id="collapse_band_1">
+                <div id="collapse_band_title"></div>
+                <div id="collapse_band_angle"></div>
+            </div>
+            <div id="collapse_band_2">
+                <p id="collapse_band_paragraph"></p>
+            </div>
+        </div>
+    </>
+}
--- my-app/src/pages/About/About.jsx
+++ my-app/src/pages/About/About.jsx
@@ -1,10 +1,12 @@
 import { Banner } from '../../components/Banner'
+import { Collapse } from '../../components/Collapse'
 import image2 from '../../images/Image2.png'
+import '../About/style/About.css'
 
 export function About() {
     return <>
         < Banner image={image2} />
-        <h1>About</h1>
+        < Collapse />
     </>
 
 }
