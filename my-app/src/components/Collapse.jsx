9be31a43acc9eede1c053f6b517761645f64deca
mohaali77 <mzr.mohand@gmail.com>
2023-07-31 01:56:18 +0200

    modif

--- my-app/src/components/Collapse.jsx
+++ my-app/src/components/Collapse.jsx
@@ -1,12 +1,12 @@
-export function Collapse() {
+export function Collapse({ title, paragraph }) {
     return <>
         <div id="collapse">
             <div id="collapse_band_1">
-                <div id="collapse_band_title"></div>
-                <div id="collapse_band_angle"></div>
+                <div id="collapse_band_title">{title}</div>
+                <div id="collapse_band_angle"><i className="fa-solid fa-angle-up"></i></div>
             </div>
             <div id="collapse_band_2">
-                <p id="collapse_band_paragraph"></p>
+                <p id="collapse_band_paragraph">{paragraph}</p>
             </div>
         </div>
     </>
