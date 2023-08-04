30b0d30cb087e5140c2ecc342dbfe249fb0ad8ab
mohaali77 <mzr.mohand@gmail.com>
2023-08-04 23:33:11 +0200

    modif

--- my-app/src/components/Collapse.jsx
+++ my-app/src/components/Collapse.jsx
@@ -1,12 +1,12 @@
 export function Collapse({ title, paragraph }) {
     return <>
-        <div id="collapse">
-            <div id="collapse_band_1">
-                <div id="collapse_band_title">{title}</div>
-                <i id="collapse_band_angle" className="fa-solid fa-angle-up"></i>
+        <div className="collapse">
+            <div className="collapse_band_1">
+                <div className="collapse_band_title">{title}</div>
+                <i className="fa-solid fa-angle-up"></i>
             </div>
-            <div id="collapse_band_2">
-                <p id="collapse_band_paragraph">{paragraph}</p>
+            <div className="collapse_band_2">
+                <p className="collapse_band_paragraph">{paragraph}</p>
             </div>
         </div>
     </>
--- my-app/src/pages/Home/Home.jsx
+++ my-app/src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import { Card } from "../../components/Card"
 import image1 from '../../images/Image1.png'
 import "../../pages/Home/style/banner_home.css"
 import "../../pages/Home/style/card.css"
+
 import data from '../../data/data.json'
 const sentance = 'Chez vous, partout et ailleurs'
 
