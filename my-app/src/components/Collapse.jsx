ff395076a0b14620faca97b1459b7c9325e37261
mohaali77 <mzr.mohand@gmail.com>
2023-08-07 13:51:57 +0200

    modif

--- my-app/src/components/Collapse.jsx
+++ my-app/src/components/Collapse.jsx
@@ -1,11 +1,23 @@
+import React, { useRef } from 'react';
+
 export function Collapse({ title, paragraph }) {
+
+    const angleBtn = useRef(null);
+    const collapseOpenClose = useRef(null);
+
+
+    const handleClick = () => {
+        angleBtn.current.classList.toggle('rotate');
+        collapseOpenClose.current.classList.toggle('collapse_band_2_show');
+    }
+
     return <>
         <div className="collapse">
             <div className="collapse_band_1">
                 <div className="collapse_band_title">{title}</div>
-                <i className="fa-solid fa-angle-up"></i>
+                <i ref={angleBtn} onClick={handleClick} className="fa-solid fa-angle-down"></i>
             </div>
-            <div className="collapse_band_2">
+            <div ref={collapseOpenClose} className="collapse_band_2">
                 <p className="collapse_band_paragraph">{paragraph}</p>
             </div>
         </div>
