abc17a17a9233e28d8c913d58316c8a9233b9fbd
mohaali77 <mzr.mohand@gmail.com>
2023-08-09 16:01:22 +0200

    modif

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,13 +1,36 @@
 import logo from '../logo/LOGO_header.png'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import { useEffect, useRef } from 'react';
 
 export function Header() {
+
+    const getPage = useLocation();
+
+    const home = useRef(null);
+    const about = useRef(null);
+
+
+    useEffect(() => {
+        if (getPage.pathname === '/') {
+            home.current.classList.add('actualPage');
+        } else {
+            home.current.classList.remove('actualPage');
+        }
+
+        if (getPage.pathname === '/about') {
+            about.current.classList.add('actualPage');
+        } else {
+            about.current.classList.remove('actualPage');
+        }
+    }, [getPage.pathname]);
+
+
     return <header>
-        <img src={logo} alt="logo de l'entreprise KASA" />
+        <Link to='/'><img src={logo} alt="logo de l'entreprise KASA" /></Link>
         <nav>
             <ul>
-                <li id='accueil'><Link to='/'>Accueil</Link></li>
-                <li id='aPropos'><Link to='/about'>A Propos</Link></li>
+                <li ref={home} id='accueil'><Link to='/'>Accueil</Link></li>
+                <li ref={about} id='aPropos'><Link to='/about'>A Propos</Link></li>
             </ul>
         </nav>
     </header>
