dc096aa27b50192cb8144e48488605b8ee687a77
mohaali77 <mzr.mohand@gmail.com>
2023-08-20 01:40:23 +0200

    Update Slideshow.jsx

--- my-app/src/components/Slideshow.jsx
+++ my-app/src/components/Slideshow.jsx
@@ -6,8 +6,10 @@ export function Slideshow({ pictures }) {
     //on définit un state réprésentant l'index initial comme 0
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
+    //on créer une variable isOnePicture que l'on définit comme false.
     let isOnePicture = false
 
+    //si le nombre de photo du tableau est égal ou inférieur à 1, alors on définis isOnePicture à true
     if (pictures.length <= 1) {
         isOnePicture = true
     }
@@ -25,12 +27,15 @@ export function Slideshow({ pictures }) {
     return <>
         <section className="slideshow_container_housedetails">
             <img src={pictures[currentImageIndex]} alt='' />
-            {isOnePicture ?
-                null
-                : <> <i onClick={handleNavigateLeft} className="fa-solid fa-angle-left"></i>
-                    <i onClick={handleNavigateRight} className="fa-solid fa-angle-right"></i>
-                    <div className="slideshow_counter">{currentImageIndex + 1}/{pictures.length}</div>
-                </>
+            {
+                //si isOnePicture = true, les boutons de navigation et le compteur ne s'affiche pas
+                //sinon on les affiche
+                isOnePicture ?
+                    null
+                    : <> <i onClick={handleNavigateLeft} className="fa-solid fa-angle-left"></i>
+                        <i onClick={handleNavigateRight} className="fa-solid fa-angle-right"></i>
+                        <div className="slideshow_counter">{currentImageIndex + 1}/{pictures.length}</div>
+                    </>
             }
         </section>
     </>
